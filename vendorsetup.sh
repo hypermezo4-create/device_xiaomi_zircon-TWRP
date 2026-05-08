0008b2839f9da813d28657a8857a2451be01d259
Mohammed-MEZO <hypermezo4@gmail.com>
2026-05-08 07:12:26 +0300

    fixed again

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,83 +18,65 @@
 # 	Please maintain this if you use this script or any part of it
 #
 
-FOX_MANIFEST_ROOT=$(gettop)
-if [ -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox_defaults.go -a -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox.mk ]; then
-    if [ -z "$TW_DEFAULT_LANGUAGE" ]; then
-        unset TW_DEFAULT_LANGUAGE
-        export TW_DEFAULT_LANGUAGE="zh_CN"
-    fi
+#set -o xtrace
+FDEVICE="zircon"
 
-    # Build
-    export LC_ALL="C"
-    export FOX_VERSION=$(date +%y.%m.%d)
-    export ALLOW_MISSING_DEPENDENCIES=true
-    export TARGET_DEVICE_ALT="zircon"
-    export FOX_VIRTUAL_AB_DEVICE=1
-    export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-    #export OF_MAINTAINER="D8100-9000-TWRP-Device-Tree Team"
+fox_get_target_device() {
+	export script_path="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+	if echo "$script_path" | grep -q "$FDEVICE"; then
+		FOX_BUILD_DEVICE="$FDEVICE"
+	elif echo "$0" | grep -q "$FDEVICE"; then
+		FOX_BUILD_DEVICE="$FDEVICE"
+	fi
+}
 
-    # Magiskboot
-    export OF_USE_MAGISKBOOT=1
-    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-    export FOX_PATCH_VBMETA_FLAG=1
-
-    # Magisk
-    export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v30.7.zip
-
-    # Binaries
-    export FOX_USE_BASH_SHELL=1
-    export FOX_ASH_IS_BASH=1
-    export FOX_USE_NANO_EDITOR=1
-    export FOX_USE_GREP_BINARY=1
-    export FOX_USE_TAR_BINARY=1
-    export FOX_USE_ZIP_BINARY=1
-    export FOX_USE_SED_BINARY=1
-    export FOX_REPLACE_TOOLBOX_GETPROP=1
-    export FOX_USE_XZ_UTILS=1
-    export FOX_REPLACE_BUSYBOX_PS=1
-    export OF_ENABLE_LPTOOLS=1
-
-    # Decryption
-    export OF_FBE_METADATA_MOUNT_IGNORE=1
-    export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-    export OF_NO_RELOAD_AFTER_DECRYPTION=1
-
-    # Screen
-    export OF_SCREEN_H=2400
-    export OF_STATUS_H=50
-    export OF_STATUS_INDENT_LEFT=85
-    export OF_STATUS_INDENT_RIGHT=85
-    export OF_HIDE_NOTCH=1
-    export OF_CLOCK_POS=1
-    export OF_ALLOW_DISABLE_NAVBAR=0
-
-    # Backup
-    export OF_QUICK_BACKUP_LIST="/boot;/vendor_boot;/data;"
-
-    # OTA & MIUI
-    export OF_NO_MIUI_PATCH_WARNING=1
-    export OF_PATCH_AVB20=1
-    # export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-
-    # Others
-    export OF_USE_GREEN_LED=0
-    export OF_FLASHLIGHT_ENABLE=0
-    export TW_MAX_BRIGHTNESS=1200
-
-    # Maintainer & Build Info
-      export FOX_MAINTAINER_PATCH_VERSION="0"
-      export OF_MAINTAINER="Mezo"
-
-    if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-        export | grep "FOX" >>$FOX_BUILD_LOG_FILE
-        export | grep "OF_" >>$FOX_BUILD_LOG_FILE
-        export | grep "TARGET_" >>$FOX_BUILD_LOG_FILE
-        export | grep "TW_" >>$FOX_BUILD_LOG_FILE
-    fi
+if [ -z "$FOX_BUILD_DEVICE" ]; then
+	fox_get_target_device
 fi
+
+if [ "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	echo "Detected build device: $FOX_BUILD_DEVICE"
+
+# Review build flags with below links:
+# https://gitlab.com/OrangeFox/vendor/recovery/-/raw/fox_14.1/orangefox_build_vars.txt
+# https://gitlab.com/OrangeFox/bootable/Recovery/-/raw/fox_14.1/orangefox.mk
+
+	# A/B Partition
+	export FOX_VIRTUAL_AB_DEVICE=1
+	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+
+	# Compression Binaries & Tools
+	export FOX_USE_BASH_SHELL=1
+	export FOX_USE_NANO_EDITOR=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_LZ4_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export FOX_USE_ZSTD_BINARY=1
+	export FOX_DELETE_AROMAFM=1
+	export FOX_REMOVE_AAPT=1
+	export FOX_USE_BUSYBOX_BINARY=1
+	export FOX_USE_GREP_BINARY=1
+
+	# KernelSU / Magisk Support
+	# export FOX_DELETE_MAGISK_ADDON=1
+	export FOX_USE_SPECIFIC_MAGISK_ZIP="$script_path/prebuilt/Magisk-v30.6.zip"
+	export FOX_MOVE_MAGISK_INSTALLER_TO_RAMDISK=1
+	export FOX_ENABLE_KERNELSU_SUPPORT=1
+	export FOX_ENABLE_KERNELSU_NEXT_SUPPORT=1
+	export FOX_ENABLE_SUKISU_SUPPORT=1
+
+	export FOX_SETTINGS_ROOT_DIRECTORY=/data/recovery
+    export FOX_MISCELLANEOUS_ROOT_DIRECTORY=/sdcard
+      
+    # Maintainer & Build Info
+    export FOX_MAINTAINER_PATCH_VERSION="0"
+    export OF_MAINTAINER="Mezo"
+else
+	echo "I: vendorsetup.sh skipped; device mismatch or environment issue."
+fi
+
   
 
 
